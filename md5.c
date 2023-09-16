249b16755d31ed777df325d55164c7f4f9b7c6f3
Francisco F <fferrutti@itba.edu.ar>
2023-09-16 12:52:11 -0300

    initial files problem solved

--- md5.c
+++ md5.c
@@ -25,7 +25,7 @@ void initialize_resources(int *shm_fd, char **shared_memory, sem_t **shm_semapho
 void create_pipes_and_children(int parent_to_child_pipe[CHILD_QTY][2], int child_to_parent_pipe[CHILD_QTY][2], pid_t child_pid[CHILD_QTY]);
 void handle_select_and_pipes(int argc, const char *argv[], int parent_to_child_pipe[CHILD_QTY][2], int child_to_parent_pipe[CHILD_QTY][2], int *files_assigned, pid_t child_pid[CHILD_QTY], char child_md5[CHILD_QTY][MAX_MD5 + MAX_PATH + 4], sem_t *shm_semaphore, sem_t *avail_semaphore, char *shared_memory, FILE *resultado_file, int *vision_opened);
 void cleanup(sem_t *shm_semaphore, sem_t *avail_semaphore, int shm_fd, pid_t child_pid[CHILD_QTY], int parent_to_child_pipe[CHILD_QTY][2]);
-int distribute_files(int argc, const char *argv[], int parent_to_child_pipe[][2], int child_to_parent_pipe[][2]);
+int distribute_initial_files(int argc, const char *argv[], int parent_to_child_pipe[][2], int child_to_parent_pipe[][2]);
 
 
 int main(int argc, const char *argv[]) {
@@ -40,6 +40,7 @@ int main(int argc, const char *argv[]) {
     int child_to_parent_pipe[CHILD_QTY][2];
     char child_md5[CHILD_QTY][MAX_MD5 + MAX_PATH + 4];
     int files_assigned;
+    argc--;//we won't hash the ./md5
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
@@ -48,11 +49,11 @@ int main(int argc, const char *argv[]) {
 
     initialize_resources(&shm_fd, &shared_memory, &shm_semaphore, &avail_semaphore, &vision_opened, &resultado_file);
     create_pipes_and_children(parent_to_child_pipe, child_to_parent_pipe, child_pid);
-    files_assigned = distribute_files(argc, argv, parent_to_child_pipe, child_to_parent_pipe);
+    files_assigned = distribute_initial_files(argc, argv, parent_to_child_pipe, child_to_parent_pipe);
     handle_select_and_pipes(argc, argv, parent_to_child_pipe, child_to_parent_pipe, &files_assigned, child_pid, child_md5, shm_semaphore, avail_semaphore, shared_memory, resultado_file, &vision_opened);
     cleanup(shm_semaphore, avail_semaphore, shm_fd, child_pid, parent_to_child_pipe);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
 
 void initialize_resources(int *shm_fd, char **shared_memory, sem_t **shm_semaphore, sem_t **avail_semaphore, int *vision_opened, FILE **resultado_file) {
@@ -128,12 +129,17 @@ void create_pipes_and_children(int parent_to_child_pipe[CHILD_QTY][2], int child
             char *args[] = {"./slave.elf", NULL};
             execve(args[0], args, NULL);
             perror("execve");
-            exit(EXIT_FAILURE);
+            
+            //when returned, all files were hased. So now we gotta close the child's file descriptors
+            
+            exit(EXIT_SUCCESS);
         }
     }
 }
 
-void handle_select_and_pipes(int argc, const char *argv[], int parent_to_child_pipe[CHILD_QTY][2], int child_to_parent_pipe[CHILD_QTY][2], int *files_assigned, pid_t child_pid[CHILD_QTY], char child_md5[CHILD_QTY][MAX_MD5 + MAX_PATH + 4], sem_t *shm_semaphore, sem_t *avail_semaphore, char *shared_memory, FILE *resultado_file, int *vision_opened) {
+void handle_select_and_pipes(int argc, const char *argv[], int parent_to_child_pipe[CHILD_QTY][2], int child_to_parent_pipe[CHILD_QTY][2], 
+int *files_assigned, pid_t child_pid[CHILD_QTY], char child_md5[CHILD_QTY][MAX_MD5 + MAX_PATH + 4], sem_t *shm_semaphore, sem_t *avail_semaphore, 
+char *shared_memory, FILE *resultado_file, int *vision_opened) {
     fd_set readfds;
     int max_fd = -1;
     int current_file_index = 0;
@@ -185,8 +191,6 @@ void handle_select_and_pipes(int argc, const char *argv[], int parent_to_child_p
 
                     if (*files_assigned < argc) {
                         pipe_write(parent_to_child_pipe[i][1], argv[(*files_assigned)++]);
-                    } else {
-                        close(parent_to_child_pipe[i][1]);
                     }
 
                     current_file_index++;
@@ -205,10 +209,13 @@ void handle_select_and_pipes(int argc, const char *argv[], int parent_to_child_p
 
 void cleanup(sem_t *shm_semaphore, sem_t *avail_semaphore, int shm_fd, pid_t child_pid[CHILD_QTY], int parent_to_child_pipe[CHILD_QTY][2]) {
     for (int i = 0; i < CHILD_QTY; i++) {
-        waitpid(child_pid[i], NULL, 0);
         close(parent_to_child_pipe[i][1]);
     }
-
+    
+    for(int i=0; i<CHILD_QTY; i++){
+        waitpid(child_pid[i], NULL, 0);
+    }
+    
     sem_close(shm_semaphore);
     sem_unlink(SHM_SEMAPHORE_NAME);
 
@@ -219,10 +226,11 @@ void cleanup(sem_t *shm_semaphore, sem_t *avail_semaphore, int shm_fd, pid_t chi
     shm_unlink(SHARED_MEMORY_NAME);
 }
 
-int distribute_files(int argc, const char *argv[], int parent_to_child_pipe[][2], int child_to_parent_pipe[][2]) {
-    int files_assigned = 0;
-    for (int child_index = 0; child_index < CHILD_QTY; child_index++) {
-        for (int i = 0; i < INITIAL_FILES_PER_CHILD; i++) {
+int distribute_initial_files(int argc, const char *argv[], int parent_to_child_pipe[][2], int child_to_parent_pipe[][2]) {
+    
+    int files_assigned = 1;
+    for(int i=0; i<INITIAL_FILES_PER_CHILD && i*CHILD_QTY<argc; i++){
+        for(int child_index=0; child_index<CHILD_QTY && i*CHILD_QTY+child_index<argc; child_index++){
             pipe_write(parent_to_child_pipe[child_index][1], argv[files_assigned++]);
         }
     }
